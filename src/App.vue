2199259b5bceb0a7cbaf8fe638b3e6a8b7b274d0
atsushi.iida <iida.atsushi.fh@gmail.com>
2024-05-03 10:20:09 +0900

    fix actions error

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {RouterView} from 'vue-router'
+import { RouterView } from 'vue-router'
 import Header from './components/OriginalHeader.vue'
 import Footer from './components/OriginalFooter.vue'
 </script>
