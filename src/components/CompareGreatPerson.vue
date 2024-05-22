f93da936ee820730d9088d2a4c8f2618a6de761b
atsushi.iida <iida.atsushi.fh@gmail.com>
2024-05-22 22:18:07 +0900

    Update CompareGreatPerson.vue

--- src/components/CompareGreatPerson.vue
+++ src/components/CompareGreatPerson.vue
@@ -80,6 +80,26 @@ const referencesSite: Ref<Array<{ link: string; text: string; accessDate: string
     link: 'https://ja.wikipedia.org/wiki/%E5%BE%B3%E5%B7%9D%E5%AE%B6%E5%BA%B7',
     text: '徳川家康 - Wikipedia',
     accessDate: '2024年05月21日'
+  },
+  {
+    link: 'https://ja.wikipedia.org/wiki/%E3%83%88%E3%83%BC%E3%83%9E%E3%82%B9%E3%83%BB%E3%82%A8%E3%82%B8%E3%82%BD%E3%83%B3',
+    text: 'エジソン - Wikipedia',
+    accessDate: '2024年05月21日'
+  },
+  {
+    link: 'https://ja.wikipedia.org/wiki/%E3%82%A8%E3%82%A4%E3%83%96%E3%83%A9%E3%83%8F%E3%83%A0%E3%83%BB%E3%83%AA%E3%83%B3%E3%82%AB%E3%83%BC%E3%83%B3',
+    text: 'エイブラハム・リンカーン - Wikipedia',
+    accessDate: '2024年05月21日'
+  },
+  {
+    link: 'https://ja.wikipedia.org/wiki/%E3%82%AB%E3%83%BC%E3%83%8D%E3%83%AB%E3%83%BB%E3%82%B5%E3%83%B3%E3%83%80%E3%83%BC%E3%82%B9',
+    text: 'カーネル・サンダース - Wikipedia',
+    accessDate: '2024年05月21日'
+  },
+  {
+    link: 'https://ja.wikipedia.org/wiki/%E3%82%A2%E3%83%AB%E3%83%99%E3%83%AB%E3%83%88%E3%83%BB%E3%82%A2%E3%82%A4%E3%83%B3%E3%82%B7%E3%83%A5%E3%82%BF%E3%82%A4%E3%83%B3',
+    text: 'アルベルト・アインシュタイン - Wikipedia',
+    accessDate: '2024年05月21日'
   }
 ])
 
